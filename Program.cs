9055a7968b2be38cc74a596375d2471778c18592
Raihan Habib <raihanhbh@yahoo.com>
2017-04-12 12:41:46 +0600

    Create my First Console Application in C#

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            // Keep the console window open in debug mode.
+            Console.WriteLine("Press any key to exit.");
+            Console.ReadKey();
+        }
+    }
+}
